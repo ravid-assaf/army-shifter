5fe7a86af46b7ea1600f735529736b4352fb4d46
ravid-assaf <ravid.assaf@gmail.com>
2025-05-15 10:17:51 +0300

    working to deploy in GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/army-shifter/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
